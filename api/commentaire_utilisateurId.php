e522f838e01ec24053fdaad671168283a70e0dde
Joris Margotteau <jorismargotteau@gmail.com>
2021-12-08 18:43:03 +0100

    add 2 services (recettes and commentaires search by userID)

---
+++ api/commentaire_utilisateurId.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+    // Se connecter à la base de données
+    include("./db_connect.php");
+    $request_method = $_SERVER["REQUEST_METHOD"];
+    $id = intval($_GET["id"]);
+    getCommentaires($id);
+
+    function getCommentaires($id)
+    {
+        global $conn;
+        $query = "SELECT * FROM commentaires
+                  WHERE auteurId = ".$id." ;";
+        $response = array();
+        $result = mysqli_query($conn, $query);
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+        {
+            $response[] = $row;
+        }
+        header('Content-Type: application/json');
+        echo json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+?>
